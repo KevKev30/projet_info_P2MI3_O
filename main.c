1e8aaa36fb1c0819e5cab38fc905c8706015f1fa
KevKev30 <kevin.nguyenoanh30@gmail.com>
2024-12-03 19:45:03 +0100

    Création des structures consommateur et station

--- main.c
+++ main.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+typedef struct consommateur{
+    int identitfiant;
+    int conso;
+}consommateur;
+
+typedef struct station{
+    int identifiant;
+    int capacite;
+    int somme_conso;
+}station;
