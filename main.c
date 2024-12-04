c5afc631ccccc8845b974e58db8fc6bd3f94982c
KevKev30 <kevin.nguyenoanh30@gmail.com>
2024-12-04 17:56:24 +0100

    Ajout de la fonction centrale

--- main.c
+++ main.c
@@ -3,13 +3,22 @@
 #include <string.h>
 #include <math.h>
 
-typedef struct consommateur{
-    int identitfiant;
-    int conso;
-}consommateur;
+typedef struct centrale{
+    int identifiant;
+    int capacite;
+}
+
 
 typedef struct station{
     int identifiant;
     int capacite;
     int somme_conso;
 }station;
+
+
+typedef struct consommateur{
+    int identitfiant;
+    int conso;
+}consommateur;
+
+
