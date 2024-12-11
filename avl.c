f991d94fb22cbdebb1378dbe5f0cd41b57333384
anaelle9 <anaellejoachim05@gmail.com>
2024-12-11 17:58:57 +0100

    Ajout fonction insertionAVL et fonction consommationTotal

--- avl.c
+++ avl.c
@@ -4,12 +4,6 @@
 #include <math.h>
 #include "avl.h"
 
-
-int estVide(AVL * a){
-    return a == NULL;
-}
-
-
 int max(int a, int b){
     if (a<b){
         return b;
@@ -36,7 +30,7 @@ int min3(int a, int b, int c){
 }
 
 
-AVL * creerAVL(station e){
+AVL * creerAVL(Station e){
     AVL * a = (AVL*) * malloc(sizeof(AVL));
     if (a==NULL){
         exit(0);
@@ -117,35 +111,32 @@ AVL * equilibreAVL(AVL * a){
     return a;
 }
 
-
-AVL * suppressionMinAVL(AVL * a, int * h, station * pe){
-    AVL * tmp;
-    if(a->fg == NULL){
-        pe->station->identifiant = a->station->identifiant;
-        pe->station->capacite = a->station->capacite;
-        pe->station->somme_conso = a->station->somme_conso;
-        pe->eq = a->eq;
-        pe->fd = a->fd;
-        pe->fg = a->fg;
-        *h = -1;
-        tmp = a;
-        a = a->fd;
-        free(tmp);
-        return a;
+AVL * insertionAVL(AVL * a, Station e, int *h){
+    if (a == NULL){
+        *h = 1;
+        return creerAVL(e);
+    }
+    else if (e->station < a->station->somme_conso){
+        a->fg = insertionAVL(a->fg, e, h);
     }
     else{
-        a->fg = suppMinAVL(a->fg, h, pe);
-        *h = -*h;
+        *h = 0;
+        return a;
     }
-    if(*h != 0){
+
+    if (*h != 0){
         a->eq += *h;
         a = equilibreAVL(a);
-        if(a->eq == 0){
-            *h = -1;
-        }
-        else{
-            *h = 0;
-        }
+        *h = (a->eq == 0) ? 0 : 1; 
     }
     return a;
+}
+
+int consommationTotal(AVL * a){
+    if (a == NULL){
+        return 0;
+    }
+    else {
+        return a->station->somme_conso + consommationTotal(a->fg) + consommationTotal(a->fd);
+    }
 }
--- avl.h
+++ avl.h
@@ -1,15 +1,15 @@
-typedef struct station{
+typedef struct Station{
     int identifiant;
     int capacite;
     int somme_conso;
-}station;
+}Station;
 
 
 typedef struct AVL{
-    station st;
+    Station station;
     int eq;
-    struct * AVL fd;
-    struct * AVL fg;
+    struct AVL * fd;
+    struct AVL * fg;
 }AVL;
 
 int estVide(AVL * a);
@@ -17,4 +17,11 @@ int max(int a, int b);
 int min(int a, int b);
 int max3(int a, int b, int c);
 int min3(int a, int b, int c);
-AVL * creerAVL(station e);
+AVL * creerAVL(Station e);
+AVL * rotationGauche(AVL * a);
+AVL * rotationDroite(AVL * a);
+AVL * doubleRotationGauche(AVL * a);
+AVL * doubleRotationDroite(AVL * a);
+AVL * equilibre(AVL * a);
+AVL * insertionAVL(AVL * a, Station e, int * h)
+int consommationTotal(AVL * a);
