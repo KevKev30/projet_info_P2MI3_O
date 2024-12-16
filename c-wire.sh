054f8df96d3de99ec8c2dd02e4d269c4e931288e
anaelle9 <anaellejoachim05@gmail.com>
2024-12-16 18:17:38 +0100

    Modification du script

--- c-wire.sh
+++ c-wire.sh
@@ -16,59 +16,6 @@ id_central=$4
 option_aide=$5
 
 
-
-
-
-    
-function choix_station(){
-    echo "Voici les differentes station :"
-    echo " hvb"
-    echo " hva"
-    echo " lv"
-    read -p "Quel station souhaitez-vous analyser ? " station
-    case $station in 
-        1) type_station="hvb";;
-        2) type_station="hva";;
-        3) type_station="lv";;
-        *) echo "La station station n'existe pas, veuillez réessayez"; choix_station ;;
-    esac
-}
-choix_station
-
-
-#fonction qui demande à l'utilisateur de choisir le consommateur qu'il veut étudier
-function choix_consommateur(){
-    echo "Voici les differents consommateur :"
-    echo " comp"
-    echo " indiv"
-    echo " all"
-    read -p "Quel consommateur souhaitez-vous analyser ? " consommateur
-    case $consommateur in 
-        1) type_consommateur="comp";;
-        2) type_consommateur="indiv";;
-        3) type_consommateur="all";;
-        *) echo "Le consommateur station n'existe pas, veuillez réessayez"; choix_consommateur ;;
-    esac
-    
-}
-choix_consommateur
-
-
-
-
-
-
-#verification options interdites
-if { [[ "$type_station" == "hvb" ]] || [[ "$type_station" == "hva" ]]; } && { [[ "$type_consommateur" == "indiv" ]] || [[ "$type_consommateur" =="all" ]]; }; then
-    echo "erreur"
-    exit 1
-fi
-
-
-
-
-
-
 #fonction qui demande à l'itulisateur de choisir le type de station qu'il veut étudier
 function choix_station(){
     echo "Voici les differentes station :"
@@ -77,7 +24,7 @@ function choix_station(){
     echo " lv"
     read -p "Quel station souhaitez-vous analyser ? " station
     case $station in 
-        hvb) echo "Vous avez choisi la station : $station" type_station="hvb";;
+        hvb) echo "Vous avez choisi la station : $station" ;;
         hva) echo "Vous avez choisi la station : $station";;
         lv) echo "Vous avez choisi la station : $station";;
         *) echo "La station $station n'existe pas, veuillez réessayez"; choix_station ;;
@@ -102,3 +49,16 @@ function choix_consommateur(){
     
 }
 choix_consommateur
+
+
+
+
+#verification options interdites
+if { [[ "$station" == "hvb" ]] || [[ "$station" == "hva" ]]; } && { [[ "$consommateur" == "indiv" ]] || [[ "$consommateur" == "all" ]]; }; then
+    echo "Attention cette option est interdites"
+    exit 1
+fi
+
+
+
+
